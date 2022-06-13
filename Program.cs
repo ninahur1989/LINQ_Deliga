d9a9bb8a3a98938f5f48bb66403639ab8879338d
stas <79771234+ninahur1989@users.noreply.github.com>
2022-06-13 13:12:14 +0300

    finish 2 task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQ_Deliga
 {
@@ -6,6 +7,26 @@ namespace LINQ_Deliga
     {
         public static void Main(string[] args)
         {
+            Iinitialition iinitialition = new Iinitialition();
+            ContactLsitcs[] list = iinitialition.Iinitialize();
+            var result0 = list.FirstOrDefault(x => x.Id == 0);
+            Console.WriteLine(result0.Name + " " + result0.Surname);
+            var result00 = list.Select(x => x.Surname.Length == 10);
+            Console.WriteLine(result00);
+            var result1 = list.Where(x => x.Name.ToLower() == x.Name);
+            foreach (ContactLsitcs result in result1)
+            {
+                Console.WriteLine(result.Name);
+            }
+
+            var result2 = list.GroupBy(x => x.Id).Reverse();
+            var result3 = list.Where(x => x.Surname.StartsWith('a'));
+            var result4 = result1.Except(result3);
+            Console.WriteLine();
+            foreach (ContactLsitcs result in result4)
+            {
+                Console.WriteLine(result.Id);
+            }
         }
     }
 }
